bb2b3b1815b4e580b4c759477adcc0b78cf7addb
abhijit <kamathabhijit2002@gmail.com>
2023-12-22 00:07:54 -0500

    learning a CRUD api in GO Lang

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# this repository is to track my learning of the Go language through the freecodecamp course
+# This repository is to track my learning of the Go language through the freecodecamp course
 
 Goals:
 - learn how to build backend systems and to explore cloud computing/hosting etc
@@ -6,3 +6,5 @@ Goals:
 My learning based on part of the complete tutorial
 1. Built a simple web server. Understood simple Go Syntax.
 2. A complete CRUD API
+    - go mod init name of mod-file (go 1.17 + requires a module file (go.mod))
+    
--- main.go
+++ main.go
@@ -1,50 +1,127 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request){
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() error: %v", err)
-		return
-	}
-
-	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address  := r.FormValue("address")
-	// fmt.Fprintf is equivalent to printing to the screen
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"` // a pointer to the director struct
 }
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r * http.Request){
+	w.Header().Set("Content-Type", "Application/json")
+	params  := mux.Vars(r)
+
+	for _, item := range movies{
+		// _ is a blank identifier to avoid the error of not using the index variable
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie 
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+
+	movies = append(movies,movie)
+
+	json.NewEncoder(w).Encode(movie)
+
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 	
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
 
-
-func helloHandler(w http.ResponseWriter, r *http.Request){
-	// w and r are pointer
-
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
 	}
 
-	if r.Method != "GET" {
-		http.Error(w,"Get method is not supported", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(w, "Hello!")
+	var movie Movie 
+
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID= params["id"]
+
+	movies = append(movies, movie)
+	
+	json.NewEncoder(w).Encode(movies)
+	return
 }
 
-func main(){
-	fileserver := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileserver)
-	http.HandleFunc("/form", formHandler) // shows the form
-	http.HandleFunc("/hello",helloHandler) // runs the hello function
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "438227",
+		Title: "Movie One",
+		Director: &Director{
+			Firstname: "Michael",
+			Lastname:  "Scorsese",
+		},
+	})
+
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "445344",
+		Title: "Movie Two",
+		Director: &Director{
+			Firstname: "Timmald",
+			Lastname:  "Fox",
+		},
+	})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
